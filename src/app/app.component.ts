bc22bfe1f40752d54a18a15866e7406472539bd0
Michal Tomaszewski <michalt48@gmail.com>
2017-08-25 09:00:43 -0700

    Fix link for a project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -97,7 +97,7 @@ export class AppComponent {
       "name": "vSphere Docker Volume Service (vDVS)",
       "image": "github_icon.svg",
       "description": "Enterprise grade, Storage and Data services for Stateful Cloud Native Applications",
-      "url": "https://vmware.github.io/vsphere-storage-for-docker/"
+      "url": "https://vmware.github.io/docker-volume-vsphere/"
     },
     {
       "name" : "tdnf",
